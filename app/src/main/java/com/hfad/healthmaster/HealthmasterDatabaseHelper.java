a134fb416b7b4c3caa2fe038588cace021a4dbab
ndosker <ndosker@mills.edu>
2020-03-28 18:31:19 -0700

    Complete initial code for adding tables to database

--- app/src/main/java/com/hfad/healthmaster/HealthmasterDatabaseHelper.java
+++ app/src/main/java/com/hfad/healthmaster/HealthmasterDatabaseHelper.java
@@ -4,8 +4,7 @@ import android.content.ContentValues;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-
-import java.sql.Date;
+//import java.sql.Date;
 
 public class HealthmasterDatabaseHelper extends SQLiteOpenHelper {
 
@@ -18,10 +17,12 @@ public class HealthmasterDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db){
+        // Create users table
         db.execSQL("CREATE TABLE USERS ("
                 + "UserID INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + "FirstName VARCHAR(45), "
                 + "LastName VARCHAR(45));");
+        // Create physical health table
         db.execSQL("CREATE TABLE PHYSICALHEALTH ("
                 + "EntryID INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + "DayID Date, "
@@ -30,7 +31,53 @@ public class HealthmasterDatabaseHelper extends SQLiteOpenHelper {
                 + "FoodID INTEGER DEFAULT NULL, "
                 + "DrinkID INTEGER DEFAULT NULL, "
                 + "FoodQty DOUBLE DEFAULT(0), "
-                + "DrinkQty DOUBLE DEFAULT(0));");
+                + "DrinkQty DOUBLE DEFAULT(0),"
+                + " FOREIGN KEY (UserID) REFERENCES USERS (UserID));");
+        // Create mental health table
+        db.execSQL("CREATE TABLE MENTALHEALTH ("
+                + "EntryID INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "DayID Date, "
+                + "UserID INTEGER NOT NULL, "
+                + "AnxID INTEGER, "
+                + "MoodID INTEGER, "
+                + "Lo5 VARCHAR(1000), "
+                + "T4 VARCHAR(1000), "
+                + "Li3 VARCHAR(1000), "
+                + "S2 VARCHAR(1000), "
+                + "DesSur VARCHAR(1000), "
+                + "MoodLevel INTEGER, "
+                + "MoodDesc VARCHAR(1000), "
+                + "MoodTrigger VARCHAR(1000) DEFAULT '', "
+                + " FOREIGN KEY (UserID) REFERENCES USERS (UserID));");
+        // Create food table
+        db.execSQL("CREATE TABLE FOOD ("
+                + "FoodID INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "FoodName VARCHAR(45),"
+                + "FoodCal DOUBLE, "
+                + "FoodFat DOUBLE, "
+                + "FoodSugar DOUBLE, "
+                + "FoodChol DOUBLE, "
+                + "FoodPotas DOUBLE, "
+                + "Microwaved TINYINT);");
+        // Create drink table
+        db.execSQL("CREATE TABLE DRINK ("
+                + "DrinkID INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "DrinkName VARCHAR(45),"
+                + "DrinkCal DOUBLE, "
+                + "DrinkSugar DOUBLE, "
+                + "DrinkSod DOUBLE);");
+        // Create food entry joining table
+        db.execSQL("CREATE TABLE FOODENTRY ("
+                + "FoodID INTEGER NOT NULL, "
+                + "DayID Date,"
+                + "UserID INTEGER NOT NULL, "
+                + "CONSTRAINT pk_eatfood PRIMARY KEY(FoodID, DayID, UserID));");
+        // Create drink entry joining table
+        db.execSQL("CREATE TABLE DRINKENTRY ("
+                + "DrinkID INTEGER NOT NULL, "
+                + "DayID Date,"
+                + "UserID INTEGER NOT NULL, "
+                + "CONSTRAINT pk_eatdrink PRIMARY KEY(DrinkID, DayID, UserID));");
     }
 
     @Override
@@ -63,5 +110,92 @@ public class HealthmasterDatabaseHelper extends SQLiteOpenHelper {
         pHealthValues.put("FoodQty", foodQ);
         pHealthValues.put("DrinkQty", drinkQ);
         db.insert("PHYSICALHEALTH", null, pHealthValues);
+    }
+
+    private static void insertMHealth(SQLiteDatabase db,
+                                      String day,
+                                      Integer user,
+                                      Integer anx,
+                                      Integer mood,
+                                      String lo5,
+                                      String t4,
+                                      String li3,
+                                      String s2,
+                                      String desSur,
+                                      Integer mLevel,
+                                      String mDesc,
+                                      String mTrigger){
+        ContentValues mHealthValues = new ContentValues();
+        mHealthValues.put("DayID", day);
+        mHealthValues.put("UserID", user);
+        mHealthValues.put("AnxID", anx);
+        mHealthValues.put("MoodID", mood);
+        mHealthValues.put("Lo5", lo5);
+        mHealthValues.put("T4", t4);
+        mHealthValues.put("Li3", li3);
+        mHealthValues.put("S2", s2);
+        mHealthValues.put("DesSur", desSur);
+        mHealthValues.put("MoodLevel", mLevel);
+        mHealthValues.put("MoodDesc", mDesc);
+        mHealthValues.put("MoodTrigger", mTrigger);
+        db.insert("MENTALHEALTH", null, mHealthValues);
+    }
+
+    private static void insertFood(SQLiteDatabase db,
+                                   Integer FoodID,
+                                   String FoodName,
+                                   Double FoodCal,
+                                   Double FoodFat,
+                                   Double FoodSugar,
+                                   Double FoodChol,
+                                   Double FoodPotas,
+                                   Boolean Microwaved){
+        ContentValues foodValues = new ContentValues();
+        foodValues.put("FoodID", FoodID);
+        foodValues.put("FoodName", FoodName);
+        foodValues.put("FoodCal", FoodCal);
+        foodValues.put("FoodFat", FoodFat);
+        foodValues.put("FoodSugar", FoodSugar);
+        foodValues.put("FoodChol", FoodChol);
+        foodValues.put("FoodPotas", FoodPotas);
+        foodValues.put("Microwaved", Microwaved);
+        db.insert("FOOD", null, foodValues);
+    }
+
+    private static void insertDrink(SQLiteDatabase db,
+                                   Integer DrinkID,
+                                   String DrinkName,
+                                   Double DrinkCal,
+                                   Double DrinkSugar,
+                                   Double DrinkSod){
+        ContentValues drinkValues = new ContentValues();
+        drinkValues.put("DrinkID", DrinkID);
+        drinkValues.put("DrinkName", DrinkName);
+        drinkValues.put("DrinkCal", DrinkCal);
+        drinkValues.put("DrinkSugar", DrinkSugar);
+        drinkValues.put("DrinkSod", DrinkSod);
+        db.insert("DRINK", null, drinkValues);
+    }
+
+    private static void insertFoodEntry(SQLiteDatabase db,
+                                        Integer FoodID,
+                                        String DayID,
+                                        Integer UserID){
+        ContentValues fEntryValues = new ContentValues();
+        fEntryValues.put("FoodID", FoodID);
+        fEntryValues.put("DayID", DayID);
+        fEntryValues.put("UserID", UserID);
+        db.insert("FOODENTRY", null, fEntryValues);
+    }
+
+    private static void insertDrinkEntry(SQLiteDatabase db,
+                                        Integer DrinkID,
+                                        String DayID,
+                                        Integer UserID){
+        ContentValues dEntryValues = new ContentValues();
+        dEntryValues.put("FoodID", DrinkID);
+        dEntryValues.put("DayID", DayID);
+        dEntryValues.put("UserID", UserID);
+        db.insert("DRINKENTRY", null, dEntryValues);
     }
 }
